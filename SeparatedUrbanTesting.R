c4a19fa5927e8e0bc7d90f2bda07604db649b7ed
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2018-09-22 18:40:25 -0400

    Update SeparatedUrbanTesting.R

--- SeparatedUrbanTesting.R
+++ SeparatedUrbanTesting.R
@@ -6,8 +6,11 @@ library(glmmADMB)
 library(qpcR)
 
 #----Loading the pfw data----
-pfw = read.csv('~/WNV F18/PFW_amecro_zerofill_landcover.csv')
+pfw = read.csv('~/Desktop/WNV2/PFW_amecro_zerofill_landcover.csv')
 pfw = pfw[pfw$state=="MA",]
+
+years = 1999:2004
+pfw = pfw[pfw$yr %in% years,]
 
 #----Converting habitat to binary for urban/not-urban----
 # Making a new column in the df
@@ -19,14 +22,12 @@ ones = pfw[pfw$urban >= 0.6,]
 
 #----Running a glm for NON-URBAN----
 glm.nU = glmmadmb(maxFlock~effortDays+effortHours+lat+long+yr+(1|locID), data=zeros,
-                         zeroInflation = T, family="nbinom")
+                  zeroInflation = T, family="nbinom")
 
 #----Running a glm for URBAN----
 glm.U = glmmadmb(maxFlock~effortDays+effortHours+lat+long+yr+(1|locID), data=ones,
-                        zeroInflation = T, family="nbinom")
+                 zeroInflation = T, family="nbinom")
 
 #----Exploring models----
 summary(glm.nU) # 1.1% decline by yr, sig p val ||| 1.9% decline with re
 summary(glm.U) # 3.6% decline by yr, sig p val ||| 2.6% decline with re
-
-# Started model at 3:05, check date()
