5e9864d1b88f696df5924a712417fe204219a161
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2018-09-22 18:35:14 -0400

    Add files via upload

---
+++ SeparatedUrbanTesting.R
@@ -0,0 +1,32 @@
+# Gates Dupont #
+# Sept 2018    #
+# # # # # # # #
+
+library(glmmADMB)
+library(qpcR)
+
+#----Loading the pfw data----
+pfw = read.csv('~/WNV F18/PFW_amecro_zerofill_landcover.csv')
+pfw = pfw[pfw$state=="MA",]
+
+#----Converting habitat to binary for urban/not-urban----
+# Making a new column in the df
+pfw$urban = pfw$URB
+
+# Separating by urban value
+zeros = pfw[pfw$urban <= 0.4,]
+ones = pfw[pfw$urban >= 0.6,]
+
+#----Running a glm for NON-URBAN----
+glm.nU = glmmadmb(maxFlock~effortDays+effortHours+lat+long+yr+(1|locID), data=zeros,
+                         zeroInflation = T, family="nbinom")
+
+#----Running a glm for URBAN----
+glm.U = glmmadmb(maxFlock~effortDays+effortHours+lat+long+yr+(1|locID), data=ones,
+                        zeroInflation = T, family="nbinom")
+
+#----Exploring models----
+summary(glm.nU) # 1.1% decline by yr, sig p val ||| 1.9% decline with re
+summary(glm.U) # 3.6% decline by yr, sig p val ||| 2.6% decline with re
+
+# Started model at 3:05, check date()
