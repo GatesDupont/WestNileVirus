7a0f7c43f8442af0030119623d462d533a0cf7ef
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2019-04-06 19:53:26 -0400

    Update AICModelComparison.R

--- final/AICModelComparison.R
+++ final/AICModelComparison.R
@@ -4,10 +4,10 @@
 
 "Model Comparison Table for ZI-GAMs"
 
-
 library(mgcv)
 library(plyr)
 library(dplyr)
+library(qpcR)
 
 #----Load clean data----
 pfw = read.csv("~/WNV SP19/PFW_amecro_clean.csv")
@@ -41,186 +41,259 @@ pfw$dummy = 1
 
 #------------------------------------------------MODELS------------------------------------------------
 
+# DOT
+#----Base model - Dot model----
+f.Psi_dot__N_dot = list(
+  maxFlock ~ 
+    dummy,
+  ~
+    dummy)
+
+# TIME
+#----Base model - 1 Time Dependence----
+f.Psi_Year__N_dot = list(
+  maxFlock ~ 
+    dummy,
+  ~
+    s(yr, k=11))
+
+#----Base model - 2 Time Dependence----
+f.Psi_dot__N_Year = list(
+  maxFlock ~ 
+    s(yr, k=11),
+  ~
+    dummy)
+
+#----Base model - 1 2 Time Dependence----
+f.Psi_Year__N_Year = list(
+  maxFlock ~ 
+    s(yr, k=11),
+  ~
+    s(yr, k=11))
+
+# LOCATION
+#----Base model - 1 Coordinates----
+f.Psi_Coord__N_dot = list(
+  maxFlock ~ 
+    dummy,
+  ~
+    s(lat,long))
+
+#----Base model - 2 Coordinates----
+f.Psi_dot__N_Coord = list(
+  maxFlock ~ 
+    s(lat,long),
+  ~
+    dummy)
+
+#----Base model - 1 2 Coordinates----
+f.Psi_Coord__N_Coord = list(
+  maxFlock ~ 
+    s(lat,long),
+  ~
+    s(lat,long))
+
+# EFFORT DAYS
+#----Base model - 1 Effort Days----
+f.Psi_Days__N_dot = list(
+  maxFlock ~ 
+    dummy,
+  ~
+    effortDays)
+
+#----Base model - 2 Effort Days----
+f.Psi_dot__N_Days = list(
+  maxFlock ~ 
+    effortDays,
+  ~
+    dummy)
+
+#----Base model - 1 2 Effort Days----
+f.Psi_Days__N_Days = list(
+  maxFlock ~ 
+    effortDays,
+  ~
+    effortDays)
+
+# EFFORT HOURS
+#----Base model - 1 Effort Hours----
+f.Psi_Hours__N_dot = list(
+  maxFlock ~ 
+    dummy,
+  ~
+    effortHours)
+
+#----Base model - 2 Effort Hours----
+f.Psi_dot__N_Hours = list(
+  maxFlock ~ 
+    effortHours,
+  ~
+    dummy)
+
+#----Base model - 1 2 Effort Hours----
+f.Psi_Hours__N_Hours = list(
+  maxFlock ~ 
+    effortHours,
+  ~
+    effortHours)
+
+
+
+#----Aggregating model formulas----
+model.formulas = list(
+  f.Psi_dot__N_dot, 
+  # TIME
+  f.Psi_Year__N_dot,
+  f.Psi_dot__N_Year,
+  f.Psi_Year__N_Year,
+  # LOCATION
+  f.Psi_Coord__N_dot,
+  f.Psi_dot__N_Coord,
+  f.Psi_Coord__N_Coord,
+  # EFF DAYS
+  f.Psi_Days__N_dot,
+  f.Psi_dot__N_Days,
+  f.Psi_Days__N_Days,
+  # EFF HOURS
+  f.Psi_Hours__N_dot,
+  f.Psi_dot__N_Hours,
+  f.Psi_Hours__N_Hours)
+
+model.names = c(
+  "Psi_dot__N_dot",
+  # TIME
+  "Psi_Year__N_dot",
+  "Psi_dot__N_Year",
+  "Psi_Year__N_Year",
+  # LOCATION
+  "Psi_Coord__N_dot",
+  "Psi_dot__N_Coord",
+  "Psi_Coord__N_Coord",
+  # EFF DAYS
+  "Psi_Days__N_dot",
+  "Psi_dot__N_Days",
+  "Psi_Days__N_Days",
+  # EFF HOURS
+  "Psi_Hours__N_dot",
+  "Psi_dot__N_Hours",
+  "Psi_Hours__N_Hours")
+
+model.fits = vector("list", 13)
+
 #----Running models in parallel----
 library(doParallel)
 cores = detectCores()
 cl = makeCluster(cores[1]-1)
 registerDoParallel(cl)
 
-#----Let's go----
-models = vector("list", 1000)
-fits = vector("list", 1000)
-traj.data = vector("list", 1000)
+model.fits = foreach(i=1:13, .packages=c("mgcv")) %dopar% {
+  #for(i in 1){    
+  assign(model.names[[i]], 
+         gam(formula = model.formulas[[i]], family = ziplss, gamma=1.4, data=pfw))
+}
 
-#...FOREACH...
-
-#----Base model - Dot model----
-Psi_dot__N_dot = gam(list(
-  maxFlock ~ 
-    dummy,
-  ~
-    dummy),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - 1 Time Dependence----
-Psi_Year__N_dot = gam(list(
-  maxFlock ~ 
-    s(yr, k=11),
-  ~
-    dummy),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - 2 Time Dependence----
-Psi_dot__N_Year = gam(list(
-  maxFlock ~ 
-    dummy,
-  ~
-    s(yr, k=11)),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - 1 2 Time Dependence----
-Psi_Year__N_Year = gam(list(
-  maxFlock ~ 
-    s(yr, k=11),
-  ~
-    s(yr, k=11)),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - 1 Coordinates----
-Psi_Coord__N_dot = gam(list(
-  maxFlock ~ 
-    s(lat,long),
-  ~
-    dummy),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - 2 Coordinates----
-Psi_dot__N_Coord = gam(list(
-  maxFlock ~ 
-    dummy,
-  ~
-    s(lat,long)),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - 1 2 Coordinates----
-Psi_Coord__N_Coord = gam(list(
-  maxFlock ~ 
-    s(lat,long),
-  ~
-    s(lat,long)),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - Effort Days----
-Psi_Days__N_Days = gam(list(
-  maxFlock ~ 
-    effortDays,
-  ~
-    effortDays),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - Effort Days----
-Psi_Days__N_Days = gam(list(
-  maxFlock ~ 
-    effortDays,
-  ~
-    effortDays),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - Effort Days----
-Psi_Days__N_Days = gam(list(
-  maxFlock ~ 
-    effortDays,
-  ~
-    effortDays),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - Effort Hours----
-Psi_Hours__N_Hours = gam(list(
-  maxFlock ~ 
-    effortHours,
-  ~
-    effortHours),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - Effort in both----
-Psi_Effort__N_Effort = gam(list(
-  maxFlock ~ 
-    effortDays + effortHours,
-  ~
-    effortDays + effortHours),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - Effort no Count----
-Psi_Effort__N_dot = gam(list(
-  maxFlock ~ 
-    dummy,
-  ~
-    effortDays + effortHours),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Base model - Effort no Detect----
-Psi_dot__N_Effort = gam(list(
-  maxFlock ~ 
-    effortDays + effortHours,
-  ~
-    dummy),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
-#----Full model----
-Psi_full__N_full = gam(list(
-  maxFlock ~ 
-    effortHours +
-    effortDays +
-    s(day, k=5) +
-    s(yr, k=11) +
-    s(lat,long),
-  ~
-    effortHours +
-    effortDays +
-    s(day, k=5) +
-    s(yr, k=11) +
-    s(lat,long)),
-  family = ziplss,
-  gamma = 1.4,
-  data = pfw)
-
+#----Assigning Names----
+for(i in 1:13){
+  assign(model.names[[i]],
+         model.fits[[i]])
+}
 
 #----AIC Model Comparison table----
-models.aic = AIC(Psi_Hab__N_Hab_noAG, Psi_Hab__N_Hab_noFOR, Psi_Hab__N_Hab_noOPEN,
-                 Psi_Hab__N_Hab_noURB, Psi_Hab__N_Hab_noWAT, Psi_Hab__N_Hab_noWET,Psi_Hab__N_Hab_noSEA,
-                 Psi_Hours__N_Hours, Psi_Days__N_Days, Psi_Coord__N_Coord, Psi_Year__N_Year, Psi_dot__N_dot, 
-                 Psi_dot__N_Hab, Psi_Hab__N_dot,
-                 Psi_Effort__N_Effort, Psi_Effort__N_dot, Psi_dot__N_Effort,
-                 Psi_full__N_full_noRE, Psi_full__N_full)
+models.aic = AIC(
+  Psi_dot__N_dot, 
+  # TIME
+  Psi_Year__N_dot,
+  Psi_dot__N_Year,
+  Psi_Year__N_Year,
+  # LOCATION
+  Psi_Coord__N_dot,
+  Psi_dot__N_Coord,
+  Psi_Coord__N_Coord,
+  # EFF DAYS
+  Psi_Days__N_dot,
+  Psi_dot__N_Days,
+  Psi_Days__N_Days,
+  # EFF HOURS
+  Psi_Hours__N_dot,
+  Psi_dot__N_Hours,
+  Psi_Hours__N_Hours)
 models.weights = akaike.weights(models.aic$AIC)
 models.aictab = cbind(models.aic,models.weights)
 models.aictab=models.aictab[order(models.aictab["deltaAIC"]),]
 View(models.aictab)
+
+#----AIC Model Comparison table----
+models.aic = AIC(
+  Psi_dot__N_dot, 
+  # TIME
+  Psi_Year__N_dot,
+  Psi_dot__N_Year,
+  Psi_Year__N_Year,
+  # LOCATION
+  Psi_Coord__N_dot,
+  Psi_dot__N_Coord,
+  Psi_Coord__N_Coord,
+  # EFF DAYS
+  Psi_Days__N_dot,
+  Psi_dot__N_Days,
+  Psi_Days__N_Days,
+  # EFF HOURS
+  Psi_Hours__N_dot,
+  Psi_dot__N_Hours,
+  Psi_Hours__N_Hours)
+models.weights = akaike.weights(models.aic$AIC)
+models.aictab = cbind(models.aic,models.weights)
+models.aictab=models.aictab[order(models.aictab["deltaAIC"]),]
+View(models.aictab)
+
+#----AIC Model Comparison table----
+models.aic = AIC(
+  Psi_dot__N_dot, 
+  # TIME
+  Psi_Year__N_dot,
+  Psi_dot__N_Year,
+  Psi_Year__N_Year)
+models.weights = akaike.weights(models.aic$AIC)
+models.aictab = cbind(models.aic,models.weights)
+models.aictab.t=models.aictab[order(models.aictab["deltaAIC"]),]
+View(models.aictab.t)
+
+#----AIC Model Comparison table----
+models.aic = AIC(
+  Psi_dot__N_dot, 
+  # LOCATION
+  Psi_Coord__N_dot,
+  Psi_dot__N_Coord,
+  Psi_Coord__N_Coord)
+models.weights = akaike.weights(models.aic$AIC)
+models.aictab = cbind(models.aic,models.weights)
+models.aictab.xy=models.aictab[order(models.aictab["deltaAIC"]),]
+View(models.aictab.xy)
+
+#----AIC Model Comparison table----
+models.aic = AIC(
+  Psi_dot__N_dot, 
+  # EFF DAYS
+  Psi_Days__N_dot,
+  Psi_dot__N_Days,
+  Psi_Days__N_Days)
+models.weights = akaike.weights(models.aic$AIC)
+models.aictab = cbind(models.aic,models.weights)
+models.aictab.days=models.aictab[order(models.aictab["deltaAIC"]),]
+View(models.aictab.days)
+
+#----AIC Model Comparison table----
+models.aic = AIC(
+  Psi_dot__N_dot, 
+  # EFF HOURS
+  Psi_Hours__N_dot,
+  Psi_dot__N_Hours,
+  Psi_Hours__N_Hours)
+models.weights = akaike.weights(models.aic$AIC)
+models.aictab = cbind(models.aic,models.weights)
+models.aictab.hrs=models.aictab[order(models.aictab["deltaAIC"]),]
+View(models.aictab.hrs)
+
+
+View(rbind(models.aictab.days, models.aictab.hrs,
+      models.aictab.t, models.aictab.xy))
