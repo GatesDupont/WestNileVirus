c85783586eddf4f8e28e69eadf684f2ba4255c54
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2019-04-09 22:46:46 -0400

    Create StudyRegionMap.R

---
+++ final/StudyRegionMap.R
@@ -0,0 +1,20 @@
+library("maps")
+library(sf)
+library(ggplot2)
+library(rnaturalearth)
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+states <- st_as_sf(map("state", plot = FALSE, fill = TRUE))
+
+selected.states = c("massachusetts", "new hampshire", "vermont", "new york", "rhode island", 
+            "connecticut", "new jersey", "maine", "pennsylvania")
+
+states$group = "gray91"
+states[states$ID %in% selected.states,]$group = "lightblue"
+
+ggplot(data = world) +
+  theme_classic() +
+  geom_sf() +
+  geom_sf(data = states, fill = states$group) + 
+  coord_sf(xlim = c(-81, -66.5), ylim = c(38.5, 47.75), expand = FALSE) +
+  theme(axis.text=element_text(size=24))
