d0651b76580f7de8ef1a3802adb2f870027b0845
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2019-04-24 16:13:50 -0400

    Update Calc_autoAIC.R

--- final/Calc_autoAIC.R
+++ final/Calc_autoAIC.R
@@ -1,5 +1,5 @@
 # Gates Dupont    #
-# April 22, 2019  #
+# April 24, 2019  #
 # # # # # # # # # #
 
 # 42837
@@ -7,80 +7,31 @@ library(mgcv)
 library(tidyr)
 library(stringr)
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# Simulating data as in original ziplss documentaiton #
-# Wood et al. 2016                                    #
-# http://dx.doi.org/10.1080/01621459.2016.1180986     #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#----Making all possible combinations of full model----
 
-#----Simulating data----
-f0 <- function(x) 2 * sin(pi * x); f1 <- function(x) exp(2 * x)
-f2 <- function(x) 0.2 * x^11 * (10 * (1 - x))^6 + 10 * 
-  (10 * x)^3 * (1 - x)^10
-n <- 500;set.seed(5)
-x0 <- runif(n); x1 <- runif(n)
-x2 <- runif(n); x3 <- runif(n)
+# hours, days, years, latlon
+count = expand.grid(count_days    = c("effortDays", "dummy"),
+                    count_hours   = c("effortHours", "dummy"),
+                    count_years   = c("s(yr, k=11)", "dummy"),
+                    count_latlon  = c("s(lat, long)", "dummy"))
 
-## Simulate probability of potential presence...
-eta1 <- f0(x0) + f1(x1) - 3
-p <- binomial()$linkinv(eta1) 
-y <- as.numeric(runif(n)<p) ## 1 for presence, 0 for absence
+detect = expand.grid(detect_days    = c("effortDays", "dummy"),
+                     detect_hours   = c("effortHours", "dummy"),
+                     detect_years   = c("s(yr, k=11)", "dummy"),
+                     detect_latlon  = c("s(lat, long)", "dummy"))
 
-## Simulate y given potentially present (not exactly model fitted!)...
-ind <- y>0
-eta2 <- f2(x2[ind])/3
-y[ind] <- rpois(exp(eta2),exp(eta2))
-
-# # # # # # # # # # # #
-# End simulated data  # 
-# # # # # # # # # # # #
-
-#----Pulling the data together----
-df = data.frame(y, x0, x1, x2, x3)
-
-# Full model, for reference
-fullModel <- gam(list(
+formulas = vector("list", 16*16)
+k = 1
+for (i in 1:length(count[,1])) {
+  a = as.character(unlist(count[i,])) %>%
+    str_flatten(collapse = " + ")
   
-  y
-  ~s(x0)+s(x1)+s(x2)+s(x3), # Count
-  ~s(x0)+s(x1)+s(x2)+s(x3)), # Detection
-  
-  family=ziplss(), 
-  data=df)
-
-#----Model selection code----
-
-# FUNCTION PARAMETERS
-# Dataframe
-# Index of covars of interest
-  # Full model will have all of these covariates in both steps
-  # All covariates will be smoothed
-
-# MAKE MODEL FORMULAS
-# Add dummy to data
-# Dummy, one predictor, two predictors...
-# Multi-covariate must have all combinations
-
-# # # # # # # # # # # # # # # # # # # # # # #
-# Reference eBird Best Practices            #
-# http://strimas.com/ebird-best-practices/  #
-# Johnston et al. 2019                      #
-# # # # # # # # # # # # # # # # # # # # # # #
-
-continuous_covs <- df %>% 
-  select(2, 3, 4) %>% 
-  names()
-
-# create model formula for predictors
-gam_formula_rhs <- str_glue("s({var}, k = {k})", 
-                            var = continuous_covs, k = 4) %>% 
-  str_flatten(collapse = " + ") %>% 
-  str_glue(" ~ ", .,
-           " + protocol_type + ",
-           "s(time_observations_started, bs = \"cc\", k = {k})", 
-           k = k_time) %>% 
-  as.formula()
-
-# model formula including response
-gam_formula <- update.formula(observation_count ~ ., gam_formula_rhs)
-gam_formula
+  for(j in 1:length(detect[,1])){
+    b = as.character(unlist(detect[j,])) %>%
+      str_flatten(collapse = " + ")
+    
+    formulas[[k]] = list(formula(str_glue("maxFlock", " ~ ", a)),
+                         formula(str_glue(" ~ ", b)))
+    k = k + 1
+  }
+}
