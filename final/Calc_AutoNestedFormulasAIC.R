7cb570b6790f4c28f299140ee1e0f20621eb0135
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2019-04-24 16:47:53 -0400

    Update Calc_AutoNestedFormulasAIC.R

--- final/Calc_AutoNestedFormulasAIC.R
+++ final/Calc_AutoNestedFormulasAIC.R
@@ -35,3 +35,41 @@ for (i in 1:length(count[,1])) {
     k = k + 1
   }
 }
+
+#----Generating model names----
+count.names = expand.grid(count_days    = c("D", "."),
+                          count_hours   = c("H", "."),
+                          count_years   = c("Y", "."),
+                          count_latlon  = c("L", "."))
+
+detect.names = expand.grid(detect_days    = c("D", "."),
+                           detect_hours   = c("H", "."),
+                           detect_years   = c("Y", "."),
+                           detect_latlon  = c("L", "."))
+
+model.names = vector("list", 16*16)
+k = 1
+for (i in 1:length(count.names[,1])) {
+  a = as.character(unlist(count.names[i,]))
+  a = paste(a, collapse = "")
+  
+  for(j in 1:length(detect.names[,1])){
+    b = as.character(unlist(detect.names[j,]))
+    b = paste(b, collapse = "")
+    
+    model.names[[k]] = str_glue("N(", a, ")", "Phi(", b, ")")
+    k = k + 1
+  }
+}
+
+#----Running models in parallel----
+library(doParallel)
+cores = detectCores()
+cl = makeCluster(cores[1]-1)
+registerDoParallel(cl)
+
+models = foreach(i=1:2, .packages=c("mgcv")) %dopar% {
+  #for(i in 1){    
+  assign(model.names[[i]], 
+         gam(formula = formulas[[i]], family = ziplss, gamma=1.4, data=df))
+}
