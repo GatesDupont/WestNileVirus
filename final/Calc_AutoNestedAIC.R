8962e92ab2bf41e784b2ebc78437745261b01b78
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2019-04-24 23:36:13 -0400

    Update Calc_AutoNestedAIC.R

--- final/Calc_AutoNestedAIC.R
+++ final/Calc_AutoNestedAIC.R
@@ -13,24 +13,28 @@ library(qpcR)
 df$dummy = 1
 
 #----Making all possible combinations of full model----
-count = expand.grid(count_days    = c("effortDays", "dummy"),
-                    count_hours   = c("effortHours", "dummy"),
-                    count_years   = c("s(yr, k=11)", "dummy"),
-                    count_latlon  = c("s(lat, long)", "dummy"))
+count = expand.grid(
+  count_days    = c("effortDays", "dummy"),
+  count_hours   = c("effortHours", "dummy"),
+  count_years   = c("s(yr, k=11)", "dummy"),
+  count_latlon  = c("s(lat, long)", "dummy")
+)
 
-detect = expand.grid(detect_days    = c("effortDays", "dummy"),
-                     detect_hours   = c("effortHours", "dummy"),
-                     detect_years   = c("s(yr, k=11)", "dummy"),
-                     detect_latlon  = c("s(lat, long)", "dummy"))
+detect = expand.grid(
+  detect_days    = c("effortDays", "dummy"),
+  detect_hours   = c("effortHours", "dummy"),
+  detect_years   = c("s(yr, k=11)", "dummy"),
+  detect_latlon  = c("s(lat, long)", "dummy")
+)
 
 formulas = vector("list", 256)
 k = 1
-for (i in 1:length(count[,1])) {
-  a = as.character(unlist(count[i,])) %>%
+for (i in 1:length(count[, 1])) {
+  a = as.character(unlist(count[i, ])) %>%
     str_flatten(collapse = " + ")
   
-  for(j in 1:length(detect[,1])){
-    b = as.character(unlist(detect[j,])) %>%
+  for (j in 1:length(detect[, 1])) {
+    b = as.character(unlist(detect[j, ])) %>%
       str_flatten(collapse = " + ")
     
     formulas[[k]] = list(formula(str_glue("maxFlock", " ~ ", a)),
@@ -40,24 +44,28 @@ for (i in 1:length(count[,1])) {
 }
 
 #----Generating model names----
-count.names = expand.grid(count_days    = c("D", "."),
-                          count_hours   = c("H", "."),
-                          count_years   = c("Y", "."),
-                          count_latlon  = c("L", "."))
+count.names = expand.grid(
+  count_days    = c("D", "."),
+  count_hours   = c("H", "."),
+  count_years   = c("Y", "."),
+  count_latlon  = c("L", ".")
+)
 
-detect.names = expand.grid(detect_days    = c("D", "."),
-                           detect_hours   = c("H", "."),
-                           detect_years   = c("Y", "."),
-                           detect_latlon  = c("L", "."))
+detect.names = expand.grid(
+  detect_days    = c("D", "."),
+  detect_hours   = c("H", "."),
+  detect_years   = c("Y", "."),
+  detect_latlon  = c("L", ".")
+)
 
 model.names = vector("list", 256)
 k = 1
-for (i in 1:length(count.names[,1])) {
-  a = as.character(unlist(count.names[i,]))
+for (i in 1:length(count.names[, 1])) {
+  a = as.character(unlist(count.names[i, ]))
   a = paste(a, collapse = "")
   
-  for(j in 1:length(detect.names[,1])){
-    b = as.character(unlist(detect.names[j,]))
+  for (j in 1:length(detect.names[, 1])) {
+    b = as.character(unlist(detect.names[j, ]))
     b = paste(b, collapse = "")
     
     model.names[[k]] = paste(str_glue("N(", a, ")", "Phi(", b, ")"))
@@ -68,17 +76,22 @@ for (i in 1:length(count.names[,1])) {
 #----Running models in parallel----
 library(doParallel)
 cores = detectCores()
-cl = makeCluster(cores[1]-1)
+cl = makeCluster(cores[1] - 1)
 registerDoParallel(cl)
 
-models = foreach(i=1:256, .packages=c("mgcv")) %dopar% {
-  assign(model.names[[i]], 
-         gam(formula = formulas[[i]], family = ziplss, gamma=1.4, data=df))
+models = foreach(i = 1:256, .packages = c("mgcv")) %dopar% {
+  assign(model.names[[i]],
+         gam(
+           formula = formulas[[i]],
+           family = ziplss,
+           gamma = 1.4,
+           data = df
+         ))
 }
 stopCluster(cl)
 
 #----Assigning Names----
-for(i in 1:256){
+for (i in 1:256) {
   assign(model.names[[i]],
          models[[i]])
 }
@@ -87,10 +100,10 @@ for(i in 1:256){
 aic.call = paste0("`", model.names, "`") %>%
   str_flatten(., ", ") %>%
   paste0("AIC(", ., ")") %>%
-  eval(parse(text=aic.call))
-models.aic = eval(parse(text=aic.call))
+  eval(parse(text = aic.call))
+models.aic = eval(parse(text = aic.call))
 models.weights = akaike.weights(models.aic$AIC)
-models.aictab = cbind(models.aic,models.weights)
-models.aictab=models.aictab[order(models.aictab["deltaAIC"]),]
+models.aictab = cbind(models.aic, models.weights)
+models.aictab = models.aictab[order(models.aictab["deltaAIC"]), ]
 models.aictab$weights = round(models.aictab$weights, 4)
 View(models.aictab)
